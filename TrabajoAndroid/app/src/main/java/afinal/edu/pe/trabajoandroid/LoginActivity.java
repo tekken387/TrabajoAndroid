507ae374368aacb80f7fd75b0ef97dd539b186e4
David Parra <tekken387@gmail.com>
2018-10-28 18:30:26 -0500

    editando ventana login
    agregando diseño ventana login, y autenticacion con firebase authentication

--- TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/LoginActivity.java
+++ TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/LoginActivity.java
@@ -2,12 +2,37 @@ package afinal.edu.pe.trabajoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    Button _btningresar;
+    EditText _txtusername;
+    EditText _txtpwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        _btningresar=findViewById(R.id.btnIngresar);
+        _txtpwd=findViewById(R.id.txtPwd);
+        _txtusername=findViewById(R.id.txtUsername);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
     }
 }
--- TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/MainActivity.java
+++ TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/MainActivity.java
@@ -1,13 +1,31 @@
 package afinal.edu.pe.trabajoandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import afinal.edu.pe.trabajoandroid.Util.FinalSharedPreferences;
+
 public class MainActivity extends AppCompatActivity {
+
+    FinalSharedPreferences prefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs=new FinalSharedPreferences(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String usuario = prefs.getString(prefs.SHARE_PREFERENCES_USUARIO,null);
+        if(usuario==null){
+            Intent intent=new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }
     }
 }
