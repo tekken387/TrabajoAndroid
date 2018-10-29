aa846ddcd786e6cd082b84d744bc6391b50a6355
David Parra <tekken387@gmail.com>
2018-10-28 19:23:05 -0500

    agregando firebase authentication
    agregando firebase authentication

--- TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/LoginActivity.java
+++ TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/LoginActivity.java
@@ -1,16 +1,24 @@
 package afinal.edu.pe.trabajoandroid;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private FirebaseAuth mAuth;
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     Button _btningresar;
     EditText _txtusername;
@@ -25,7 +33,10 @@ public class LoginActivity extends AppCompatActivity {
         _txtpwd=findViewById(R.id.txtPwd);
         _txtusername=findViewById(R.id.txtUsername);
 
-        mAuth = FirebaseAuth.getInstance();
+        auth = auth.getInstance();
+        user=auth.getCurrentUser();
+
+        _btningresar.setOnClickListener(this);
     }
 
     @Override
@@ -33,6 +44,36 @@ public class LoginActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+        //updateUI(currentUser);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    public void login(String email, String password){
+        auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    //Log.d(TAG, "signInWithEmail:success");
+                    FirebaseUser user = auth.getCurrentUser();
+                    //updateUI(user);
+                } else Toast.makeText(LoginActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+
+                // [START_EXCLUDE]
+                if (!task.isSuccessful()) {
+                    mStatusTextView.setText(R.string.auth_failed);
+                }
+                hideProgressDialog();
+                // [END_EXCLUDE]
+            }
+        })
     }
 }
--- TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/MainActivity.java
+++ TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/MainActivity.java
@@ -3,13 +3,20 @@ package afinal.edu.pe.trabajoandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
 
 import afinal.edu.pe.trabajoandroid.Util.FinalSharedPreferences;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     FinalSharedPreferences prefs;
-
+    Button _btnlogout;
+    FirebaseUser user;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +24,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         prefs=new FinalSharedPreferences(this);
+
+        _btnlogout=findViewById(R.id.btnLogout);
+
+        _btnlogout.setOnClickListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        String usuario = prefs.getString(prefs.SHARE_PREFERENCES_USUARIO,null);
-        if(usuario==null){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            //se queda en la pantalla main
+        } else {
             Intent intent=new Intent(this,LoginActivity.class);
             startActivity(intent);
+        }
+
+        //String usuario = prefs.getString(prefs.SHARE_PREFERENCES_USUARIO,null);
+        /*if(usuario==null){
+            Intent intent=new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }*/
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogout:
+                user=auth.getInstance().getCurrentUser();
+                auth.signOut();
         }
     }
 }
