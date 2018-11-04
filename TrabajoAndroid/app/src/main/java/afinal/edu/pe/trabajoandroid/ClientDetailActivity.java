3f7601e1d33837148ca70536fde58140fa83b782
manuel.huerto <manuel.tohuer@gmail.com>
2018-11-03 19:32:00 -0500

    Creación de Activity
    Se han creado todas las actividades que deberíamos usar:
    Cliente (Listar, Crear, Editar, Ver detalle)
    Vehiculos (Listar, Crear, Editar, Ver detalle)
    Servicios (Ver)
    Ordenes de Servicio (Listar, Crear, Editar, Ver detalle)

---
+++ TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/ClientDetailActivity.java
@@ -0,0 +1,13 @@
+package afinal.edu.pe.trabajoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClientDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_detail);
+    }
+}
