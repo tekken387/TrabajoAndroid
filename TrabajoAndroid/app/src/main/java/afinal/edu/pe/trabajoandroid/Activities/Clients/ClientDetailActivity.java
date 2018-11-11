5b4ffb39dbbe68a1410a91b5193af087bcdc38a0
David Parra <tekken387@gmail.com>
2018-11-11 17:28:13 -0500

    editting

--- TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/Activities/Clients/ClientDetailActivity.java
+++ TrabajoAndroid/app/src/main/java/afinal/edu/pe/trabajoandroid/Activities/Clients/ClientDetailActivity.java
@@ -7,9 +7,18 @@ import afinal.edu.pe.trabajoandroid.R;
 
 public class ClientDetailActivity extends AppCompatActivity {
 
+    String id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_detail);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras == null) {
+            id= null;
+        } else {
+            id= extras.getString("idcliente");
+        }
     }
 }
